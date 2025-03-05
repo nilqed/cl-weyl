5cb6524094770a9fab66b166bbf66e87d93c407c
Kurt Pagani <nilqed@gmail.com>
2025-03-05 23:37:19 +0100

    Create formal.lisp

---
+++ load+test/formal.lisp
@@ -0,0 +1,67 @@
+(ql:quickload :weyl)
+
+(in-package :weyl)
+(weyl::ge-vars '(i j k n a x))
+(defvar lb1 (eqn= i 0))
+(defvar ub1 n)
+(defvar ai (add-subscripts a i))
+(defvar body1 (* ai (expt x i)))
+
+
+(defpackage formal-objects
+  ;(:nicknames fobj :FORMAL-OBJ)
+  (:use common-lisp)  
+  ;(:import-from "WEYL") 
+  (:export "latex")) 
+
+
+(in-package :formal-objects)
+
+(defclass sum ()
+  ((lb   :initarg :lb    :initform nil :accessor sum-lb)
+   (ub   :initarg :ub    :initform nil :accessor sum-ub)
+   (body :initarg :body  :initform nil :accessor sum-body)))
+   
+
+;weyl::lb1
+;i = 0
+;(describe weyl::lb1)
+
+(defvar sum1 (make-instance 'sum  :lb weyl::lb1 
+                                  :ub weyl::ub1
+                                  :body weyl::body1))
+                                  
+#|                            
+* (in-package :formal-objects)
+#<PACKAGE "FORMAL-OBJECTS">
+* sum1
+#<SUM {10030B4EF3}>
+* (describe sum1)
+#<SUM {10030B4EF3}>
+  [standard-object]
+
+Slots with :INSTANCE allocation:
+  LB                             = i = 0
+  UB                             = n
+  BODY                           = x^i a(i)
+*    
+
+(describe weyl::lb1)
+i = 0
+  [standard-object]
+
+Slots with :INSTANCE allocation:
+  DOMAIN                         = #<Domain: GENERAL-EXPRESSIONS>
+  SIMPLIFIED?                    = NIL
+  LHS                            = i
+  RHS                            = 0
+*
+(cl-user::Type-of  weyl::lb1)
+WEYLI::GE-EQN=
+|#
+
+
+
+
+
+   
