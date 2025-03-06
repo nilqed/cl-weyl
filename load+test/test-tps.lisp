b813f8bc2ecc260d17f1369e9b127d65e119c8cc
Kurt Pagani <nilqed@gmail.com>
2025-03-06 22:53:08 +0100

    Update test-tps.lisp

--- load+test/test-tps.lisp
+++ load+test/test-tps.lisp
@@ -1,8 +1,10 @@
-;(load "load-weyl")
+(load "load-weyl")
 ;; necessary to create number system (get-rational-integers) ...
-;(compile-file "../weyl/numbers/numbers.lisp")
-;(compile-file "../weyl/tpower.lisp")
-(ql:quickload :weyl)
+(compile-file "../weyl/numbers/numbers.lisp")
+(compile-file "../weyl/tpower.lisp")
+
+
+;(ql:quickload :weyl)
 
 (in-package :weyl)
 
@@ -17,5 +19,8 @@
 (taylor (+-> "1/(1-p^2)")  tsdp 6)
 (taylor (+-> "1/(1-p^2)^4")  tsdp 6)
 
- 
+(taylor (+-> "cos(p)/(1-p^2)^4")  tsdp 12)
+;1 + (7/2)p^2 + (193/24)p^4 + (10919/720)p^6 + (1024577/40320)p^8 + 
+; (142682759/3628800)p^10 + (27509446273/479001600)p^12 + o(p^12)
+
 
