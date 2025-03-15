dfe10251765e71af0e823ef293a237a0f06c99eb
Kurt Pagani <nilqed@gmail.com>
2025-03-15 21:01:44 +0100

    Update ge-simplify.lisp

--- weyl/new/ge-simplify.lisp
+++ weyl/new/ge-simplify.lisp
@@ -11,67 +11,31 @@
 ;(y + x)^3 + 2 (1 - q^2)^-1 p + a^r
 ;* (expand ge1)
 ;3 y x^2 + y^3 + 3 y^2 x + x^3 + a^r
-; where is 2*p/(1-q^2) ????
-;(ge-equal ge1 ge2)
-; T
-;* (ge-equal ge1 (expand ge2))
-;NIL
 
-; severe bug ********
-;(expand (+-> "1/(1-q^2)"))
-; 0
-; 
-;(expand (+-> "2*p/(1-q^2)"))
-;0
+(print (ge-equal ge1 ge2))
 
-; (wtype  (+-> "1/(1-q^2)"))
-; WEYLI::GE-EXPT
-; ==> (defmethod expand ((exp ge-expt)) l. 1545
-; 
-#|
-(defmethod expand ((exp ge-expt))
-  (let ((base (base-of exp))
-	(exponent (exponent-of exp)))
-    (cond ((and (ge-plus? base)
-		(typep exponent 'rational-integer))
-	   (simp-plus-terms (domain-of exp)
-                            (expand-binomial-form (terms-of base)
-                                                  (integer-value exponent))))
-	  ((ge-times? base)
-	   (expand (simp-times-terms (domain-of exp)
-                                     (loop for term in (terms-of base)
-                                           collect (expand (expt term exponent))))))
-	  (t exp))))
-      
-|#
+(defvar g12 (- (expand ge1) ge2))
+(defvar sg12 (simplify g12))
 
-(defvar gx1 (+-> "1/(1-q^2)"))
-(defvar egx1 (exponent-of gx1))
-;-1
-(defvar bgx1 (base-of gx1))
-;1 - q^2
+(defvar tg12 (terms-of g12))
 
-(ge-plus? bgx1)
-;T
+;(loop for s in tg12 do
+;  (format t "term: ~45A   type: ~A ~%" s (wtype s)))
+  
+(defun print-terms (ge &key (ll 65))
+  "Print a table of the terms of the instance."
+  (format t "~%Terms of: ~A~%" (wtype ge))
+  (format t "~v@{~A~:*~}~%" ll "=")
+  (format t "~3A ~45A ~A ~%" "#" "Term" "Type")
+  (format t "~v@{~A~:*~}~%" ll "-")   
+  (let ((terms (terms-of ge))
+        (nr 0))
+          (loop for s in terms do
+             (format t "~3A ~45A ~A ~%" (incf nr) s (wtype s))))
+  (format t "~v@{~A~:*~}~%" ll "-"))
+              
 
-(typep egx1 'rational-integer)
-;T
+  
 
-;(simp-plus-terms (domain-of exp)
-;    (expand-binomial-form (terms-of base)
-;        (integer-value exponent)))
 
-(weyli::integer-value egx1)
-;-1
 
-(weyli::expand-binomial-form (terms-of bgx1) (weyli::integer-value egx1))
-; NIL --- that's bad
-
-(weyli::domain-of gx1)
-;#<Domain: GENERAL-EXPRESSIONS>
-
-(weyli::simp-plus-terms (weyli::domain-of gx1) nil)
-; 0
-
-;;; error :: (typep exponent 'rational-integer)) 
-;;; exponent must be positive, better >=2 of course ....
